4a47d617e2e5b2c27f658ae7528946af82cb5ebd
Emma Hayward <emmahayward93@gmail.com>
2023-02-15 18:46:30 +0000

    Added inquirer prompt questions to ask user for readMe content

--- index.js
+++ index.js
@@ -3,10 +3,41 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = [
+// array of questions for user - inside inquirer function
+const userQuestions = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'project-Title',
+            message: 'What is your project title?', 
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Write a description for your project',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'How can your project be installed?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How do you use your project?',
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'Credit other contributors or resources:',
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'What license does your project have?',
+        }
 
-];
+]);
 
 // function to write README file
 function writeToFile(fileName, data) {
